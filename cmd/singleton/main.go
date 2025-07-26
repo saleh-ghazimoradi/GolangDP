da92727a73cd53fe13e330aadd9e6396794d81c5
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-07-26 16:48:47 +0330

    Add usage examples for Singleton pattern in Golang

--- cmd/singleton/main.go
+++ cmd/singleton/main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/saleh-ghazimoradi/GolangDP/singleton"
+	"log/slog"
 )
 
 func main() {
 	s := singleton.GetInstance()
 	s.SetData("Hello World")
 	fmt.Println(s.GetData())
+
+	// Get the singleton Config instance
+	config := singleton.GetConfig()
+	fmt.Printf("Database URL: %s\n", config.DatabaseURL)
+	fmt.Printf("API Key: %s\n", config.ApiKey)
+
+	// Get the singleton DBPool instance
+	dbPool := singleton.GetDBPool()
+	err := dbPool.Db.PingContext(context.Background())
+	if err != nil {
+		fmt.Printf("Database connection failed: %v\n", err)
+		return
+	}
+	fmt.Println("Database connection successful")
+	
+	// Get the singleton Logger instance
+	logger := singleton.GetLogger()
+	logger.Log.Info("Application started successfully", slog.String("app", "my-app"))
+
 }
---
+++ singleton/useCases.go
@@ -0,0 +1,70 @@
+package singleton
+
+import (
+	"database/sql"
+	"log/slog"
+	"os"
+	"sync"
+)
+
+// Using Singleton pattern for the config setup
+var (
+	configInstance *Config
+	configOnce     sync.Once
+)
+
+type Config struct {
+	DatabaseURL string
+	ApiKey      string
+}
+
+func GetConfig() *Config {
+	configOnce.Do(func() {
+		configInstance = &Config{
+			DatabaseURL: "postgres://postgres:postgres@localhost/postgres?sslmode=disable",
+			ApiKey:      "your-api-key",
+		}
+	})
+	return configInstance
+}
+
+// Using Singleton pattern for DB pool setup
+var (
+	dbInstance *DBPool
+	dbOnce     sync.Once
+)
+
+type DBPool struct {
+	Db *sql.DB
+}
+
+func GetDBPool() *DBPool {
+	dbOnce.Do(func() {
+		db, err := sql.Open("postgres", "")
+		if err != nil {
+			panic(err)
+		}
+		dbInstance = &DBPool{
+			db: db,
+		}
+	})
+	return dbInstance
+}
+
+// Using Singleton pattern to implement logging service in Golang
+
+var (
+	logInstance *Logger
+	logOnce     sync.Once
+)
+
+type Logger struct {
+	Log *slog.Logger
+}
+
+func GetLogger() *Logger {
+	logOnce.Do(func() {
+		logInstance = &Logger{log: slog.New(slog.NewJSONHandler(os.Stdout, nil))}
+	})
+	return logInstance
+}
